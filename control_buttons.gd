b5319d26971751cfd1c62f2a68f94927c4e5f28b
m1n1tea <sir.makurov@ya.ru>
2024-03-23 15:01:16 +0300

    API for WASD

--- control_buttons.gd
+++ control_buttons.gd
@@ -1,15 +1,40 @@
 extends Control
 
 @export var button_name: String
-@onready var text_label: RichTextLabel = $ColorRect/RichTextLabel
+@onready var text_label: RichTextLabel = $Panel/RichTextLabel
+@onready var stylebox : StyleBox
 
 func _ready():
+	stylebox=StyleBoxFlat.new()
+	stylebox.set_corner_radius_all(3)
+	stylebox.set_border_width_all(3)
+	stylebox.border_color=Color(0,0,0)
+	$Panel["theme_override_styles/panel"]=stylebox
+	unhighlight()
+
+
+func unhighlight():
+	stylebox.bg_color=Color(0.745, 0.745, 0.745)
+	text_label.clear()
 	text_label.push_color(Color(255,255,255))
 	text_label.push_outline_color(Color(0,0,0))
-	text_label.push_outline_size(10)
+	text_label.push_outline_size(4)
+	text_label.push_font_size(16)
 	text_label.append_text("[center]")
 	text_label.append_text(button_name)
-	text_label.pop()	
-	text_label.pop()
-	text_label.pop()
-	text_label.pop()
+	text_label.pop_all()
+	
+func highlight():
+	stylebox.bg_color=Color(0.874, 0.941, 0.996)
+	text_label.clear()
+	text_label.push_color(Color(255,255,255))
+	text_label.push_outline_color(Color(0,0,0))
+	text_label.push_outline_size(8)
+	text_label.append_text("[center]")
+	text_label.push_font_size(17)
+	text_label.append_text(button_name)
+	text_label.pop_all()	
+	
+
+
+
