6391e2b3eb841e54db46dd13d70a1348b52ffb67
dueip <68518940+dueip@users.noreply.github.com>
2024-03-23 20:04:30 +0500

    ?

--- minigames/coffee_minigame.gd
+++ minigames/coffee_minigame.gd
@@ -1,3 +1,28 @@
 extends Minigame
 
+var is_in_mingame = false
+@onready var bobber = $Fill/Bobber
+
 	
+func get_sprite3d_aabb(sprite3d: Sprite3D) -> AABB:
+	return sprite3d.get_aabb()
+	
+func nextTurn(input: String):
+	if not is_in_mingame:
+		return
+	if (input == combination[0]):
+		bobber.position.y += 1
+	if (input == combination[1]):
+		bobber.position.y -= 1
+
+func _process(delta):
+	if not is_in_mingame:
+		return	
+	#bobber.position.y += 1
+
+func _on_minigame_started(Minigame):
+	is_in_mingame = true
+
+
+func _on_area_3d_body_exited(body):
+	print(body)
