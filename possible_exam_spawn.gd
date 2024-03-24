8250163420e85961e959bd7b1ebe25a0cff18e30
dueip <68518940+dueip@users.noreply.github.com>
2024-03-24 20:08:46 +0500

    fix: Починить теперь работает нажатие на экзамен

--- possible_exam_spawn.gd
+++ possible_exam_spawn.gd
@@ -9,13 +9,14 @@ var exam_instance
 func _ready():
 	$ExamQueue.wait_time = exams.padding()
 	$ExamQueue.start()
+	$ExamQueue.one_shot = true
 	$Area3D.monitoring = false
 
 
 
 func _input(event):
-	if $Area3D.monitoring && event is InputEventMouseButton && event.button_index == MOUSE_BUTTON_LEFT:
-		
+	if $Area3D.monitoring && event is InputEventMouseButton && event.button_index == MOUSE_BUTTON_LEFT && is_mouse_in:
+		print(current_exam.exam_name)
 		var SceneManager = get_tree().get_first_node_in_group("SceneManager")
 		SceneManager.chane_scene_to_instance(exam_instance)
 
@@ -23,31 +24,34 @@ func _on_area_3d_mouse_entered():
 	is_mouse_in = true
 
 func _on_minigame_won():
+	self.hide()
 	var SceneManager = get_tree().get_first_node_in_group("SceneManager")
-	print("we won!")
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	get_tree().get_first_node_in_group("SceneManager").revertScene()
+	current_exam = null
+	is_mouse_in = false
+	$Area3D.monitoring = false
+	queue_free()
 
-
+	
 func _on_area_3d_mouse_exited():
 	is_mouse_in = false
 
 
 func _on_waiting_for_new_exam_timeout():
-	
-	$Area3D.monitoring = true
-	$Label3D.text = current_exam.exam_name	
 	var new_exam: Exam = Exam.new()
 	new_exam.exam_name = "IT"
 	new_exam.min_wait_time = 8
 	new_exam.max_wait_time = 10
 	exams.append_possible_exams(new_exam)
 
+	
+
 
 func _on_exam_queue_timeout():
-	
+	self.show()
 	current_exam = exams.get_random_exam()
 	exam_instance = exam_minigame.instantiate()
 	exam_instance.spawning_place = self
-	$WaitingForNewExam.wait_time = randf_range(current_exam.min_wait_time, current_exam.max_wait_time)
-	$WaitingForNewExam.start()
+	$Label3D.text = current_exam.exam_name
+	$Area3D.monitoring = true
